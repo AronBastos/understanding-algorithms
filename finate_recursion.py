967e26f65766889c92250d116d3a96d71affb1cd
Aron <aron.lopes1@outlook.com>
2024-03-12 23:40:44 -0300

    AboutRecursion

---
+++ finate_recursion.py
@@ -0,0 +1,7 @@
+def loop_finito(n):
+    while n > 0:
+        print(n)
+        n -= 1
+
+# Testando o loop finito
+loop_finito(5)
---
+++ infinity_recursion.py
@@ -0,0 +1,12 @@
+def recursao_infinita(n):
+    print(n)
+    recursao_infinita(n + 1)
+
+# Testando a recursão infinita
+recursao_infinita(1)
+def recursao_infinita(n):
+    print(n)
+    recursao_infinita(n + 1)
+
+# Testando a recursão infinita
+recursao_infinita(1)
